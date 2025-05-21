bc194871419f1dba6157ab06f8328c0886ae443e
uei <uei@tomsksoft.com>
2025-05-21 09:05:03 +0300

    Release 2.4.2

--- Package.swift
+++ Package.swift
@@ -16,9 +16,8 @@ let package = Package(
         .target(name: "TSVBPackage", dependencies: ["TSVB"]),
         .binaryTarget(
             name: "TSVB", 
-            url: "https://github.com/EffectsSDK/swift-video-effects-sdk/releases/download/2.4.1/TSVB.xcframework-2.4.1.112582.zip",
-            checksum: "ea1f7b25f916021f18ccf73600583a327a16e919c54a047bea9fe1e57c7986cf"
-
+            url: "https://github.com/EffectsSDK/swift-video-effects-sdk/releases/download/2.4.2/TSVB.xcframework-iOS-2.4.2.112606-stable.zip",
+            checksum: "898ec98dd8ebe2023bba630e6e47610d8ae423905ae2f65704ea399a60ea2ced"
         )
     ]
 )
